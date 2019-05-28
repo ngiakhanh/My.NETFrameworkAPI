c4609d37ebca027487987ee9f86a5ff48115ffdb
ngiakhanh <ngiakhanh96@gmail.com>
2019-05-28 11:10:00 +0700

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace My.NETWebAPI.Controllers
+{
+    [RoutePrefix("products")]
+    public class ProductsController : ApiController
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public enum Widgets
+        {
+            Bolt,
+            Screw,
+            Nut,
+            Motor
+        }
+
+        // GET: api/Products/Widget/xxx
+        [HttpGet, Route("Widget/{widget}")]
+        public string GetProductsWithWidget(Widgets widget)
+        {
+            return "widget-" + widget;
+        }
+
+        // GET: api/Products
+        [HttpGet, Route("")]
+        public IEnumerable<string> GetAllProducts()
+        {
+            return new string[] { "product1", "product2" };
+        }
+
+        // GET: api/Products/5
+        [HttpGet, Route("{id:int:range(1000,3000)}")]
+        public string GetProduct(int id)
+        {
+            return "product-" + id;
+        }
+
+        // GET: api/Products/5/orders/custid
+        [HttpGet, Route("{id:int:range(1000,3000)}/orders/{custId}")]
+        public string GetProductForCustomer(int id, string custId)
+        {
+            return "product-orders-" + custId;
+        }
+
+        // POST: api/Products
+        [HttpPost, Route("")]
+        public void CreateProduct([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        [HttpPut, Route("{id:int:range(1000,3000)}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete, Route("{id:int:range(1000,3000)}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EnumerationConstraint/EnumerationConstraint.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace My.NETWebAPI.EnumerationConstraint
+{
+    public class EnumerationConstraint : IHttpRouteConstraint
+
+    {
+        /// <summary>
+        /// Holds the type of the Enum class to validate against
+        /// </summary>
+        public readonly Type EnumType;
+
+        /// <summary>
+        /// Constructor taking a namespace-qualified type name of the Enum type to use
+        /// </summary>
+        public EnumerationConstraint(string type)
+        {
+            var t = GetType(type);
+
+            if (t == null || !t.IsEnum)
+                throw new ArgumentException("Argument is not an Enum type", "type");
+            EnumType = t;
+        }
+
+        /// <summary>
+        /// Internal method to convert the string enum type name into a Type instance
+        /// by checking all of the currently loaded assemblies
+        /// </summary>
+        private static Type GetType(string typeName)
+        {
+            var type = Type.GetType(typeName);
+            if (type != null) return type;
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = a.GetType(typeName);
+                if (type != null)
+                    return type;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// IHttpRouteConstraint.Match implementation to validate a parameter against
+        /// the Enum members.  String comparison is NOT case-sensitive.
+        /// </summary>
+        public bool Match(HttpRequestMessage request, IHttpRoute route, string parameterName,
+            IDictionary<string, object> values, HttpRouteDirection routeDirection)
+        {
+            object value;
+
+            if (values.TryGetValue(parameterName, out value) && value != null)
+            {
+                var stringVal = value as string;
+                if (!String.IsNullOrEmpty(stringVal))
+                {
+                    // see if we can find the string in the enumeration members
+                    stringVal = stringVal.ToLower();
+                    if (null != EnumType.GetEnumNames().FirstOrDefault(a => a.ToLower().Equals(stringVal)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            throw new HttpResponseException(new HttpResponseMessage()
+            {
+                StatusCode = (HttpStatusCode)431,
+                Content = new StringContent("your text")
+            });
+            //return false;
+        }
+    }
+}
